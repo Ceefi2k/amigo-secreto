3210a652ad2c50947f34070bed00e8e8f5cb05bc
Ceefi (Fernando) <ceefiedits@gmail.com>
2025-02-06 21:59:32 -0300

    Desafio Concluído

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let lista = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value.trim();
+
+    if (nome === "") {
+        alert('Por favor, insira um nome.');
+        return;
+    }
+
+    lista.push(nome);
+    atualizarLista();
+
+    document.getElementById('amigo').value = '';
+}
+
+function atualizarLista() {
+    let listaNomes = document.getElementById('listaAmigos');
+    listaNomes.innerHTML = "";
+
+    lista.forEach((nome) => {
+        let item = document.createElement('li');
+        item.textContent = nome;
+        listaNomes.appendChild(item);
+    });
+}
+
+function sortearAmigo() {
+    if (lista.length === 0) {
+        alert('Não há amigos para sortear! Adicione amigos primeiro.');
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * lista.length);
+
+    let amigoSorteado = lista[indiceAleatorio];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = 'O amigo secreto sorteado é: <strong>' + amigoSorteado;
+}
